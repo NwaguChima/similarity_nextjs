215e78c38b31fc634c3d746ab202ba9ead159ab2
nwagu chima <chima.nwagu@decagon.dev>
2023-04-02 23:29:03 +0100

    completed paragraph

--- src/app/components/ui/Paragraph.tsx
+++ src/app/components/ui/Paragraph.tsx
@@ -1,5 +1,6 @@
 import React, { forwardRef, HTMLAttributes } from 'react';
 import { cva, VariantProps } from 'class-variance-authority';
+import { cn } from '@/app/lib/utils';
 
 const paragraphVariants = cva(
   'max-w-prose text-slate-700 dark:text-slate-300 mb-2 text-center',
@@ -23,11 +24,17 @@ interface ParagraphProps
 const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
   ({ className, size, children, ...props }, ref) => {
     return (
-      <p ref={ref} {...props} className={paragraphVariants(props)}>
+      <p
+        ref={ref}
+        {...props}
+        className={cn(paragraphVariants({ size, className }))}
+      >
         {children}
       </p>
     );
   }
 );
+
+Paragraph.displayName = 'Paragraph';
 
 export default Paragraph;
