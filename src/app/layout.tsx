1a124b031384d44d41489bf42622ce69063ddeec
nwagu chima <chima.nwagu@decagon.dev>
2023-04-02 23:51:27 +0100

    setup providers

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,9 @@
+import Providers from '@/components/Providers';
+import { cn } from '@/lib/utils';
 import '@/styles/globals.css';
+import { Inter } from 'next/font/google';
+
+const inter = Inter({ subsets: ['latin'] });
 
 export default function RootLayout({
   children,
@@ -6,8 +11,13 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={cn('bg-white text-slate-900 antialiased', inter.className)}
+    >
+      <body className="min-h-screen bg-slate-50 dark:bg-slate-900 antialiased">
+        <Providers>{children}</Providers>
+      </body>
     </html>
   );
 }
