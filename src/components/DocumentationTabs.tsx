31d36f12d198ba00431497e83b56e780ab4b4bcd
nwagu chima <chima.nwagu@decagon.dev>
2023-04-05 00:25:23 +0100

    setup doc tabs

---
+++ src/components/DocumentationTabs.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+interface DocumentationTabsProps {}
+
+const DocumentationTabs: React.FC<DocumentationTabsProps> = ({}) => {
+  return <div>DocumentationTabs</div>;
+};
+
+export default DocumentationTabs;
