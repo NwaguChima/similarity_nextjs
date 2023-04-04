f40a6304edca496d25f1774d5a76377e3df04b29
nwagu chima <chima.nwagu@decagon.dev>
2023-04-05 00:18:37 +0100

    setup doc page

---
+++ src/app/(docs)/documentation/page.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Documentation',
+  description: 'Free & open-source text similarity API',
+};
+
+const page: React.FC = () => {
+  return <div className="container max-w-7xl mx-auto mt-12">page</div>;
+};
+
+export default page;
