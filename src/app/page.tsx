f156f156b77766187544a79be9090868ca561326
nwagu chima <chima.nwagu@decagon.dev>
2023-04-04 23:31:50 +0100

    aded meta data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,11 @@
 import Image from 'next/image';
 import { Inter } from 'next/font/google';
-import styles from './page.module.css';
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Similarity API | Home',
+  description: 'Free & open-source text similarity API',
+};
 
 const inter = Inter({ subsets: ['latin'] });
 
