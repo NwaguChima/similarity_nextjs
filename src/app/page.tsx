794e431a2af74546fc41942e5561cfe212f29bcb
nwagu chima <chima.nwagu@decagon.dev>
2023-04-04 23:38:15 +0100

    setup header component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
 import { Inter } from 'next/font/google';
 import type { Metadata } from 'next';
+import LargeHeading from '@/components/ui/LargeHeading';
 
 export const metadata: Metadata = {
   title: 'Similarity API | Home',
@@ -10,5 +10,16 @@ export const metadata: Metadata = {
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main></main>;
+  return (
+    <div className="relative h-screen flex items-center justify-center overflow-x-hidden">
+      <div className="container pt-32 max-w-7xl mx-auto w-full h-full">
+        <div className="h-full gap-6 flex flex-col justify-start lg:justify-center items-center lg:items-start">
+          <LargeHeading
+            size="lg"
+            className="three-d text-black dark:text-light-gold"
+          ></LargeHeading>
+        </div>
+      </div>
+    </div>
+  );
 }
