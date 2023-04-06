8bdc7ab0ff44e7af7805f167a21ffe98687e0593
nwagu chima <chima.nwagu@decagon.dev>
2023-04-07 00:33:17 +0100

    setup auth google options

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,5 +1,38 @@
 import { NextAuthOptions } from 'next-auth';
+import { db } from '@/lib/db';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+import GoogleProvider from 'next-auth/providers/google';
+
+function getGoogleCredentials() {
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+
+  if (!clientId || clientId.length === 0) {
+    throw new Error('GOOGLE_CLIENT_ID is not defined');
+  }
+
+  if (!clientSecret || clientSecret.length === 0) {
+    throw new Error('GOOGLE_CLIENT_SECRET is not defined');
+  }
+
+  return {
+    clientId,
+    clientSecret,
+  };
+}
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(db),
+  session: {
+    strategy: 'jwt',
+  },
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: getGoogleCredentials().clientId,
+      clientSecret: getGoogleCredentials().clientSecret,
+    }),
+  ],
 };
