6eb8fc1806226ffa4d2100040d57e4277979dfd3
nwagu chima <chima.nwagu@decagon.dev>
2023-04-07 02:07:27 +0100

    setup logic for dashboard page

--- src/app/(dashboard)/dashboard/page.tsx
+++ src/app/(dashboard)/dashboard/page.tsx
@@ -1,9 +1,32 @@
 import React from 'react';
 
-interface pageProps {}
+import type { Metadata } from 'next';
+import { authOptions } from '@/lib/auth';
+import { getServerSession } from 'next-auth';
+import { notFound } from 'next/navigation';
+import { db } from '@/lib/db';
 
-const page: React.FC<pageProps> = ({}) => {
-  return <div>page</div>;
+export const metadata: Metadata = {
+  title: 'Similarity API | Dashboard',
+  description: 'Free & Open Source API for text similarity',
+};
+
+const page = async () => {
+  const user = await getServerSession(authOptions);
+
+  if (!user) return notFound();
+
+  const apikey = await db.apikey.findFirst({
+    where: {
+      userId: user.user.id,
+      enabled: true,
+    },
+  });
+  return (
+    <div className="max-w-7xl mx-auto mt-16">
+      {apikey ? <ApiDashboard /> : <RequestApikey />}
+    </div>
+  );
 };
 
 export default page;
