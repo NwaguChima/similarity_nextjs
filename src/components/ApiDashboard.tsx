99e3d244471f0c7bb5614212f0099a6da36ca11b
nwagu chima <chima.nwagu@decagon.dev>
2023-04-15 21:58:14 +0100

    added jsx

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -34,7 +34,22 @@ const ApiDashboard = async () => {
     timestamp: formatDistance(new Date(request.timestamp), new Date()),
   }));
 
-  return <div>ApiDashboard</div>;
+  return (
+    <div className="container flex flex-col gap-6">
+      <LargeHeading>Welcome back, {user.user.name}</LargeHeading>
+      <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
+        <Paragraph>Your API key:</Paragraph>
+        <Input className="w-fit truncate" readOnly value={activeApiKey.key} />
+        <ApiKeyOptions apiKeyKey={activeApiKey.key} />
+      </div>
+
+      <Paragraph className="text-center md:text-left mt-4 -mb-4">
+        Your API history:
+      </Paragraph>
+
+      <Table userRequests={serializableRequests} />
+    </div>
+  );
 };
 
 export default ApiDashboard;
