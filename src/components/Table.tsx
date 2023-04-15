3e2a3daf3d6798a0ee50b1db26e9b14cdb679a61
nwagu chima <chima.nwagu@decagon.dev>
2023-04-15 22:14:50 +0100

    completed table

--- src/components/Table.tsx
+++ src/components/Table.tsx
@@ -1,4 +1,14 @@
-import { type GridColDef } from '@mui/x-data-grid';
+'use client';
+
+import { createTheme, ThemeProvider } from '@mui/material';
+import {
+  DataGrid,
+  GridColumnHeaderParams,
+  type GridColDef,
+} from '@mui/x-data-grid';
+import { ApiRequest } from '@prisma/client';
+import { useTheme } from 'next-themes';
+import { FC } from 'react';
 
 const columnsDraft: GridColDef[] = [
   {
@@ -16,3 +26,69 @@ const columnsDraft: GridColDef[] = [
   { field: 'col4', headerName: 'Duration', width: 150 },
   { field: 'col5', headerName: 'Status', width: 150 },
 ];
+
+const columns = columnsDraft.map((col) => {
+  if (col.field === 'col1') {
+    return col;
+  }
+
+  return {
+    ...col,
+    renderHeader(params: GridColumnHeaderParams<any, any, any>) {
+      return (
+        <strong className="font-semibold">{params.colDef.headerName}</strong>
+      );
+    },
+  };
+});
+
+type ModifiedRequestType<K extends keyof ApiRequest> = Omit<ApiRequest, K> & {
+  timestamp: string;
+};
+interface TableProps {
+  userRequests: ModifiedRequestType<'timestamp'>[];
+}
+
+const Table: FC<TableProps> = ({ userRequests }) => {
+  const { theme: applicationTheme } = useTheme();
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: applicationTheme === 'light' ? 'light' : 'dark',
+    },
+  });
+
+  const rows = userRequests.map((request) => ({
+    id: request.id,
+    col1: request.usedApikey,
+    col2: request.path,
+    col3: `${request.timestamp} ago`,
+    col4: `${request.duration} ms`,
+    col5: request.status,
+  }));
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <DataGrid
+        style={{
+          backgroundColor: applicationTheme === 'light' ? 'white' : '#152238',
+          fontSize: '1rem',
+        }}
+        pageSizeOptions={[5]}
+        disableRowSelectionOnClick
+        autoHeight
+        initialState={{
+          pagination: {
+            paginationModel: {
+              pageSize: 5,
+            },
+          },
+        }}
+        columns={columns}
+        rows={rows}
+      />
+    </ThemeProvider>
+  );
+};
+
+export default Table;
