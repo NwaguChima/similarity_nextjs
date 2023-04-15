9e4fe13448de6a4f11aa7a3cf6521712add78677
nwagu chima <chima.nwagu@decagon.dev>
2023-04-15 22:17:01 +0100

    setup key otpions

--- src/components/ApiDashboard.tsx
+++ src/components/ApiDashboard.tsx
@@ -44,7 +44,7 @@ const ApiDashboard = async () => {
       <div className="flex flex-col md:flex-row gap-4 justify-center md:justify-start items-center">
         <Paragraph>Your API key:</Paragraph>
         <Input className="w-fit truncate" readOnly value={activeApiKeys.key} />
-        {/* <ApiKeyOptions apiKeyKey={activeApiKeys.key} /> */}
+        <ApiKeyOptions apiKeyKey={activeApiKeys.key} />
       </div>
 
       <Paragraph className="text-center md:text-left mt-4 -mb-4">
